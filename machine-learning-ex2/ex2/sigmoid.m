109cf0d8933df0df19de1e2c0258f09fb773bacc
mikel <amilburu@elipse.it>
2018-05-05 20:02:16 +0200

    Sigmoid and plot

--- machine-learning-ex2/ex2/sigmoid.m
+++ machine-learning-ex2/ex2/sigmoid.m
@@ -2,17 +2,28 @@ function g = sigmoid(z)
 %SIGMOID Compute sigmoid function
 %   g = SIGMOID(z) computes the sigmoid of z.
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 g = zeros(size(z));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
 
+%Get matrix, vector or scalar
 
+cols = size(z,2)
+rows = size(z,1)
 
-
-
+if (cols>1)
+  for i=1:rows
+    for j=1:cols
+      g(i,j) = 1/(1+exp(-z(i,j)));
+    endfor
+    endfor
+elseif
+  g = 1/(1 + exp(-z));
+endif
+g
 % =============================================================
 
 end
