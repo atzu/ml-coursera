2bc32f72a868441c7e4a6bb1a97e8ba883678cc6
mikel <amilburu@elipse.it>
2018-05-05 20:04:33 +0200

    Vectorize sigmoid

--- machine-learning-ex2/ex2/sigmoid.m
+++ machine-learning-ex2/ex2/sigmoid.m
@@ -11,18 +11,23 @@ g = zeros(size(z));
 
 %Get matrix, vector or scalar
 
-cols = size(z,2)
-rows = size(z,1)
+%cols = size(z,2)
+%rows = size(z,1)
 
-if (cols>1)
-  for i=1:rows
-    for j=1:cols
-      g(i,j) = 1/(1+exp(-z(i,j)));
-    endfor
-    endfor
-elseif
-  g = 1/(1 + exp(-z));
-endif
+%if (cols>1)
+%  for i=1:rows
+%    for j=1:cols
+%      g(i,j) = 1/(1+exp(-z(i,j)));
+%    endfor
+%    endfor
+%elseif
+%  g = 1/(1 + exp(-z));
+%endif
+
+%%%%% VECTORIZE %%%%%%
+
+g = 1 ./ (1 + e.^-z);
+
 g
 % =============================================================
 
