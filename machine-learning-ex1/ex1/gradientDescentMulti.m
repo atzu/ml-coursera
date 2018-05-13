43b6bb86ed9de32efeadf28f81cd393de06a4364
mikel <amilburu@elipse.it>
2018-05-13 22:56:59 +0200

    Refactor

--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -17,20 +17,13 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-    h=0;
-    theta_old = theta;
-    %for i = 1:size(X,2), % matrix column number
-      %x = X(:,i);
-      %h = h + theta_old(i)*x;
-      %theta(i) = theta_old(i) - alpha/m * sum((h -y).*x);
-    %endfor
 
 
 
     % ============================================================
 
     % Save the cost J in every iteration
-    theta = theta_old .- (alpha/m).* X' *(X*theta_old - y) 
+    theta = theta_old .- (alpha/m).* X' *(X*theta_old - y)
     J_history(iter) = computeCostMulti(X, y, theta);
 
 
